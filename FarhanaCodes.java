66d2361adc65a8e881eb4b38b36157895c6c4c11
farhana13-afk <farhanaashiq01@gmail.com>
2024-10-28 13:38:01 -0400

    Part 1
    Square Array Method Added

--- FarhanaCodes.java
+++ FarhanaCodes.java
@@ -1,5 +1,34 @@
-public class FarhanaCodes {
-	public static void main (String [] args) {
+import java.util.*;
+public class FarhanaCodes
+{
+	public static void main (String [] args)
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.println("What size array you want? ");
+		int size = reader.nextInt();
+		//Part 1
+		SquareArray(size);
 
+	}
+
+	public static void SquareArray(int size)
+	{
+		int[][] squareArray = new int[size][size];
+		for(int i=0; i<size; i++)
+		{
+			for(int j=0; j<size; j++)
+			{
+				squareArray[i][j] = (int)(Math.random() * 9)+ 1;
+			}
+		}
+
+		for(int i=0; i<size; i++)
+		{
+			for(int j=0; j<size; j++)
+			{
+				System.out.print(squareArray[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
